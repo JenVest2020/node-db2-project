6def36ae57e878515ddd847d9f3eb20347536562
JenVest2020 <jenvest2019@gmail.com>
2020-09-18 21:22:49 -0500

    2 stretches completed & exit ticket also

--- routers/carsRouter.js
+++ routers/carsRouter.js
@@ -21,4 +21,33 @@ router.post('/', async (req, res) => {
     }
 });
 
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    try {
+        const count = await db('carSpecs').update(changes).where({ id });
+        if (count) {
+            res.json({ update: count });
+        } else {
+            res.status(404).json({ message: 'invalid id' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'error updating record', error: err });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const count = await db('carSpecs').where({ id }).del();
+        if (count) {
+            res.json({ deleted: count });
+        } else {
+            res.status(404).json({ message: 'invalid id' });
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'error deleting record', error: err });
+    }
+});
+
 module.exports = router;
